c0627784eb92df4bff22d4c62f327c56cc2054b9
Dani Wahyudi <115544693+DaniWahyudi77@users.noreply.github.com>
2022-11-01 17:44:15 +0700

    Add files via upload

---
+++ UTS-Pbo-Dani/src/Bank.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class Bank {
+    int saldo = 100000;
+    
+    public void simpanUang(int jumlah){
+        saldo += jumlah;
+        System.out.println("Simpan uang: Rp."+jumlah);
+        getSaldo();
+    }
+    
+    public void ambilUang(int jumlah){
+        System.out.println("Ambil uang: Rp."+jumlah);
+        
+        if(saldo >= jumlah){
+            saldo -= jumlah;
+            getSaldo();   
+        } else {
+            System.out.println("Saldo anda tidak mencukupi");
+            getSaldo();
+        }
+    }
+    
+    public void getSaldo(){
+        System.out.println("Sisa saldo saat ini: Rp."+saldo);
+    }
+}
